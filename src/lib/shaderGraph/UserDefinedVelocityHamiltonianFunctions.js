8f805fea2b67adbd74d2412586dda9c841129d34
willsharpless <williamsharpless.2@gmail.com>
2024-09-29 14:26:36 -0700

    Control and Disturbance code default

--- src/lib/shaderGraph/UserDefinedVelocityHamiltonianFunctions.js
+++ src/lib/shaderGraph/UserDefinedVelocityHamiltonianFunctions.js
@@ -3,10 +3,11 @@ import snoise from './parts/simplex-noise';
 import {getInputUniforms} from './customInput';
 
 export default class UserDefinedVelocityHamiltonianFunctions extends BaseShaderNode {
-  constructor(updateCode, updateHamiltonianCode) {
+  constructor(updateCode, updateHamiltonianCode, updateInputCode) {
     super();
     this.updateCode = updateCode || '';
     this.updateHamiltonianCode = updateHamiltonianCode || '';
+    this.updateInputCode = updateInputCode || '';
   }
 
   setNewUpdateCode(newUpdateCode) {
@@ -15,6 +16,10 @@ export default class UserDefinedVelocityHamiltonianFunctions extends BaseShaderN
 
   setNewUpdateHamiltonianCode(newUpdateHamiltonianCode) {
     this.updateHamiltonianCode = newUpdateHamiltonianCode;
+  }
+
+  setNewUpdateInputCode(newUpdateInputCode) {
+    this.updateInputCode = newUpdateInputCode;
   }
 
   getDefines() {
@@ -60,15 +65,45 @@ float audio(float index) {
   return rgba[3];
 }
 
-${this.updateCode ? this.updateCode : 'vec2 get_velocity(vec2 p) { return vec2(0.003); }'}
+${this.updateCode ? this.updateCode : 'vec2 get_velocity(vec2 x) { return vec2(0.003); }'}
 
 ${this.updateHamiltonianCode ? this.updateHamiltonianCode : `
-  float get_hamiltonian(vec2 x, vec2 p, float t) { 
+  float get_hamiltonian(vec2 x, vec2 p, float t, float val) { 
     vec2 f = get_velocity(x);
+    // vec2 h = p * (get_velocity(x, t) + get_control(x, t, p, val) + get_disturbance(x, t, p, val));
     // return - 0.01 * (f.x + f.y);
-    return 0.01 * cos(t);
-  }`}
+    return 0.1 * cos(t);
+  }`
+}
 
+${this.updateInputCode ? this.updateInputCode : `
+
+  // Input Parameters (Linear By Default)
+  mat2 control_map = mat2(1., 0., 0., 1.);
+  vec2 control_max_mag = vec2(1., 1.);
+  mat2 disturbance_map = mat2(1., 0., 0., 1.);
+  vec2 disturbance_max_mag = vec2(0.5, 0.5);
+
+  // Input Laws (Max by Default)
+  vec2 control_law(vec2 x, float t, vec2 p, vec2 val) {
+    return control_max_mag; // debug
+  }
+  vec2 disturbance_law(vec2 x, float t, vec2 p, vec2 val) {
+    return disturbance_max_mag; // debug
+  }
+
+  // Final Input
+  vec2 get_control(vec2 x, float t, vec2 p, vec2 val) { 
+    return control_map * control_law(x, t, p, val);
+  }
+  vec2 get_disturbance(vec2 x, float t, vec2 p, vec2 val) { 
+    return disturbance_map * disturbance_law(x, t, p, val);
+  }
+
+  // if the input map nonlinear/time-varying, 
+  // you must define control_jac and disturbance_jac fns here
+`
+}
   `
   }
 }
